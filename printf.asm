9f500575b428125ec250d070ca9b00c5c8c1c32b
root <root@7078407-nv802365.twc1.net>
2026-03-23 07:44:52 +0000

    Первая версия - %c и %x

---
+++ printf.asm
@@ -0,0 +1,138 @@
+section .text
+    global _start
+
+_start:
+    push 0x01
+
+
+    mov rsi, original_str   ; original str address
+    mov rdi, str_copy       ; copy address
+
+copy_str:
+    mov al, [rsi]           ; symbol in al
+
+    cmp al, '%'
+    jne common_symbol
+    inc rsi                 ;rsi++
+
+    mov al, [rsi]           ;specifier in al
+    pop rdx                 ;number in rdx
+    call specifier_handler
+    jmp if_end
+
+common_symbol:
+    mov [rdi], al           ; copy one symbol
+    inc rdi                 ; rdi++
+
+if_end:
+    test al, al             ; if '\0'
+    jz copy_done
+
+    inc rsi                 ; rsi++
+    jmp copy_str
+
+copy_done:
+    mov rsi, str_copy       ; copy address
+    mov rdi, str_copy       ; copy begin(to cmp symbol abd '\0')
+    xor rcx, rcx            ; len counter
+
+len_calculate:
+    cmp byte [rdi], 0       ; if '\0'
+    je print_str
+    inc rcx                 ; len counter++
+    inc rdi                 ; next symabol
+    jmp len_calculate
+
+print_str:
+    mov rax, 1              ; syscall number
+    mov rdi, 1              ; stdout
+    mov rdx, rcx            ; str len
+    syscall                 ; print str
+
+    mov rax, 60
+    xor rdi, rdi            ; return 0
+    syscall                 ; end  of programm
+;-------------------------------------------------
+;Prints number as str in specifier type
+;Entry: al = specifier symbol
+;       rdi --> place to print
+;       rdx = symbol to print
+;-------------------------------------------------
+specifier_handler:
+    cmp al, 'c'
+    je char
+    cmp al, 'x'
+    je hexadecimal
+    cmp al, 'b'
+    je binar
+    cmp al, 'o'
+    je octal
+
+
+char:
+    mov [rdi], dl
+    inc rdi
+    jmp specifier_handler_end
+hexadecimal:
+    call hex_to_str
+    jmp specifier_handler_end
+binar:
+octal:
+
+specifier_handler_end:
+    ret
+;-------------------------------------------------
+;Tranform number to hex str
+;Entry: rdx  = number
+;       rdi --> place to write
+;Exit: no exit
+;Destr: rax, rcx
+;-------------------------------------------------
+hex_to_str:
+    push rdi
+
+    mov byte [rdi], '0'
+    inc rdi
+    mov byte [rdi], 'x'
+    inc rdi
+
+    mov rax, rdx
+    xor rcx, rcx
+
+    test rax, rax
+    jnz count_digits
+    mov cl, 1               ; if zero need one number
+    jmp print_prepare
+
+count_digits:
+    inc cl                  ; cl++
+    shr rax, 4              ; rax /= 16
+    test rax, rax
+    jnz count_digits
+
+print_prepare:
+    add rdi, rcx
+    dec rdi
+    push rcx                    ;save rcx in stack
+
+print_hex:
+    mov rax, rdx
+    and rax, 0xf                ;last symbol
+    mov bl, [numbers+rax]
+    mov [rdi], bl
+    dec rdi
+    shr rdx, 4
+    loop print_hex
+
+    pop rcx
+    pop rdi
+    add rdi, 2
+    add rdi, rcx
+
+    ret
+
+
+section .data
+    original_str db 'Hello, %x World!', 0xa, 0   ; исходная строка с символом новой строки и нулевым терминатором
+    str_copy     times 64 db 0                ; буфер для копии строки (64 байта)
+    numbers      db '0123456789abcdef'
